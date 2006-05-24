bff8b3612cfefc84f2ea174b006694d188973dd5
cohen <>
2006-05-24 16:27:04 +0000

    first import of calibTkrUtil

---
+++ src/mainpage.h
@@ -0,0 +1,11 @@
+// (Special "header" just for doxygen)
+
+/*! @mainpage  Package calibTkrUtil
+
+@author Hiro Tajim, Mutsumi Sugizaki, Johann Cohen-Tanugi
+
+This package gathers classes needed to preprocess TKR information when building the summary ntuple, either SVAC in EngineeringModel world or BeamTestTuple in BeamTest
+world. These classes are used to build histograms and to gather runtime information for use in later calibration/trending.
+
+
+*/
