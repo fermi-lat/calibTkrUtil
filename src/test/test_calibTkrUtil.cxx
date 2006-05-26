8702c56ca6447a597f7c60f1f50abcdc06a1e68d
cohen <>
2006-05-26 01:35:33 +0000

    add a test executable

---
+++ src/test/test_calibTkrUtil.cxx
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "RootTestAnalyzer.h"
+
+// If there is no argument, use AnalyzerOption.dat as input, otherwise use 
+// first argument as the input
+
+int main(int argc, char** argv)
+{
+  std::string optionFileName;
+  if(argc == 1) {
+    optionFileName = "../src/test/AnalyzerOption.dat";
+  }
+  else {
+    optionFileName = argv[1];
+  }
+
+  RootAnalyzer analyzer;
+
+  analyzer.parseOptionFile(optionFileName.c_str());
+
+  analyzer.analyzeData();
+
+  analyzer.produceOutputFile();
+
+}
+
