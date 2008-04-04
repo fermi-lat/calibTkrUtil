06160d6abe8f3d95b7fdb45e338c961fba077cd8
htajima <>
2008-04-04 03:03:58 +0000

    initial release of root cint library for TKR performance monitor define Gaussain convolved Landau function and return TF1*

---
+++ src/tkrPyRoot/tkrPyRoot.h
@@ -0,0 +1,21 @@
+
+#ifndef tkrPyRoot_h
+#define tkrPyRoot_h
+
+#include <string>
+#include <vector>
+
+#include "TMath.h"
+#include "TH1.h"
+#include "TF1.h"
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TFile.h"
+
+
+double langaufun(double *, double *);
+double langau2fun(double *, double *);
+double add( double, double );
+TF1* defLangau( char*, Double_t, Double_t);
+
+#endif
