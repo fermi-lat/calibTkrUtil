90fa53a46e5275e43c4f485cbc82111f09503008
htajima <>
2008-04-04 03:03:58 +0000

    initial release of root cint library for TKR performance monitor define Gaussain convolved Landau function and return TF1*

---
+++ src/tkrPyRoot/LinkDef.h
@@ -0,0 +1,16 @@
+
+#ifdef __MAKECINT__
+
+#pragma link off all class;
+#pragma link off all function;
+#pragma link off all global;
+#pragma link off all typedef;
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ function langaufun;
+#pragma link C++ function add;
+#pragma link C++ function defLangau;
+
+#endif
