504c68ff9c57d9217b97a40039962a16d29de98d
htajima <>
2008-05-23 17:05:35 +0000

    avoid memory leak by cleaning up PyROOT objects after the use.

--- python/mergeTkrRootFiles.py
+++ python/mergeTkrRootFiles.py
@@ -1,5 +1,5 @@
 # import python standard libraries
-import sys, os
+import sys, os, math, time
 
 sys.path = [( os.path.join( os.environ.get( "ROOTSYS" ), "lib" ) )] + sys.path
 
@@ -29,7 +29,8 @@ def findObjects( dir, (dirMap, objMap) ):
       if not objMap.has_key(name): objMap[name] = cname
     else: print "unknown object: %s, %s" % (name, cname)
     obj = tlist.After( obj )
-  print dir.GetName(), len(objMap)
+  tlist.Delete()
+  #print dir.GetName(), len(objMap)
 
 
 #**********************************
@@ -37,17 +38,24 @@ def findObjects( dir, (dirMap, objMap) ):
 #**********************************
 def mergeObjects( objMap, dir, inFiles ):
   #print "+++ %s +++" % dir.GetName(), objMap.keys()
-  print "# of objects in %s: %d" % ( dir.GetName(), len(objMap) )
+  #print "# of objects in %s: %d" % ( dir.GetName(), len(objMap) )
   for name in objMap.keys():
     cname = objMap[name]
     #print dir.GetName(), name, cname
     if cname == "TTree":
+      print "merge", name
       tlist = ROOT.TList()
       for inFile in inFiles:
-        tlist.Add( inFile.FindObjectAny( name ) )
+        tree = inFile.FindObjectAny( name )
+        if tree: tlist.Add( tree )
       dir.cd()
-      tree = ROOT.TTree.MergeTrees( tlist )
-      tree.Write()
+      try:
+        tree = ROOT.TTree.MergeTrees( tlist )
+        tree.Write()
+        tlist.Delete()
+        print name, "saved."
+      except:
+        print "merge erorr:", name
     else:
       dir.cd()
       hist = None
@@ -57,6 +65,7 @@ def mergeObjects( objMap, dir, inFiles ):
         if thist != None:
           if hist == None: hist = thist.Clone( name )
           else: hist.Add( thist )
+          thist.Delete()
             
       if hist != None: hist.Write()
       else: print "%s missing." % name
@@ -65,17 +74,17 @@ def mergeObjects( objMap, dir, inFiles ):
 #**********************************
 # find obejects in the directory
 #**********************************
-def mergeDirectories( dirMap, dir, inFiles ):
-  #print "--- %s ---" % dir.GetName(), dirMap.keys()
-  print "# of dirs in %s: %d" % ( dir.GetName(), len(dirMap) )
+def mergeDirectories( dirMap, dir, inFiles, path="" ):
+  #print "--- %s ---" % path, dirMap.keys()
+  #print "# of dirs in %s: %d" % ( dir.GetName(), len(dirMap) )
   for key in dirMap.keys():
-    print "***** %s *****" % key
     tdir = dir.mkdir( key )
     ( tdirMap, objMap ) = dirMap[key]
+    print "***** %s: %d %d *****" % (path+":"+key, len(objMap), len(tdirMap) )
     mergeObjects( objMap, tdir, inFiles )
-    mergeDirectories( tdirMap, tdir, inFiles )
+    mergeDirectories( tdirMap, tdir, inFiles, path+":"+key )
   dir.cd()
-  print "==== %s completed. ====" % dir.GetName()
+  print "==== %s completed. ====" % path
 
 
 
@@ -91,24 +100,94 @@ if __name__ == '__main__':
   # decode command arguments
   # [out root file] [input root files]
   #
+  t0 = time.time()
+  maxFiles = 100
+  tdir = "."
+  oname = None
+  fnames = []
   if len(sys.argv) > 2:
-    oname = sys.argv[1]
-    outRoot = ROOT.TFile( oname, "RECREATE" )
-    inFiles = []
-    (dirMap, objMap) = ( {}, {} )
-    for iname in sys.argv[2:]:
-      if not os.path.exists( iname ):
-        print "file %s does not exist." % iname
-        sys.exit()
-      inFiles.append( ROOT.TFile( iname ) )
-      findObjects( inFiles[-1], (dirMap, objMap) )
-  else:
-    print "usage: mergeTkrRootFiles.py [output] [input1] [input2]..."
+    iarg = 1
+    while iarg < len(sys.argv):
+      if sys.argv[iarg] == "-tdir":
+        iarg += 1
+        tdir = sys.argv[iarg]
+        print "temporary directory:", tdir
+      elif sys.argv[iarg] == "-max":
+        iarg += 1
+        maxFiles = int(sys.argv[iarg])
+        print "max files:", maxFiles
+      elif oname == None:
+        oname = sys.argv[iarg]
+        print "output file:", oname
+      else:
+        iname = sys.argv[iarg]
+        if not os.path.exists( iname ):
+          print "file %s does not exist." % iname
+          sys.exit()
+        fnames.append( iname )
+      iarg += 1
+        
+  if oname == None or len(fnames)==0:
+    print "usage: mergeTkrRootFiles.py [options] [output] [input1] [input2]..."
+    print " [options] include -tdir for temporary directory"
+    print " and -max for maximum files per process"
     sys.exit()
 
-  mergeObjects( objMap, outRoot, inFiles )
-  mergeDirectories( dirMap, outRoot, inFiles )
+  #
+  # analyze directory structure and find all objects in all files
+  #
+  (dirMap, objMap) = ( {}, {} )
+  for iname in fnames:
+    inFile = ROOT.TFile( iname )
+    findObjects( inFile, (dirMap, objMap) )
+    inFile.Close()
 
+  #
+  # if total # of input files are larger than maximum
+  # input files are divided.
+  #
+  inFiles = []
+  tnames = []
+  if len(fnames) > maxFiles:
+    ts = time.strftime("%y%m%d%H%M%S", time.gmtime() )
+    ndiv = int(len(fnames)/maxFiles) + 1
+    for idiv in range(ndiv):
+      kf = idiv*len(fnames)/ndiv
+      kl = (idiv+1)*len(fnames)/ndiv
+      print "merge", fnames[kf:kl]
+      print "# of files", len(fnames[kf:kl])
+      tFiles = []
+      for fname in fnames[kf:kl]:
+        tFiles.append( ROOT.TFile( fname ) )
+      tname = os.path.join( tdir, "temp-%s-%d.root"%(ts,idiv) )
+      tnames.append( tname )
+      tRoot = ROOT.TFile( tname, "RECREATE" )
+      mergeObjects( objMap, tRoot, tFiles )
+      mergeDirectories( dirMap, tRoot, tFiles, "TEMP%d"%idiv )
+      tRoot.Close()
+      for tFile in tFiles: tFile.Close()
+      print "Elapsed time: %.1f seconds" % (time.time()-t0) 
+    fnames = tnames
+
+  #
+  # produce final output
+  #
+  for fname in fnames:
+    inFiles.append( ROOT.TFile( fname )  )
+
+  print "# of files to merge", len(inFiles)
+  outRoot = ROOT.TFile( oname, "RECREATE" )
+  mergeObjects( objMap, outRoot, inFiles )
+  mergeDirectories( dirMap, outRoot, inFiles, "OUT" )
+
+  print "close output file:", outRoot.GetName()
   outRoot.Close()
+  print "close input files"
   for inFile in inFiles: inFile.Close()
+
+  if len(tnames)>0:
+    print "clean up temporary files"
+    for tname in tnames:
+      os.remove( tname )
   
+  print "Elapsed time: %.1f seconds" % (time.time()-t0) 
