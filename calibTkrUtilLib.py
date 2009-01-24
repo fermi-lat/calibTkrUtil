6c9ee6b8586af1aef2c7579b543df5fa6536ec08
cvs2git <>
2009-01-24 21:40:07 +0000

    This commit was manufactured by cvs2svn to create tag 'GlastRelease-15-49-00'.
    Sprout from GlastReleasev15r49 2008-11-19 21:21:31 UTC ecephas 'Changed'
    Cherrypick from master 2009-01-24 21:40:06 UTC ecephas 'Changed the scons version string from calibTkrUtil-02-07-03 to calibTkrUtil-02-07-03-01 to reflect the patch made (v2r7p3gl1) to get scons to build this package for GlastRelease-15-49-00':
        SConscript
        calibTkrUtilLib.py
        src/tkrPyRoot.i

---
+++ calibTkrUtilLib.py
@@ -0,0 +1,11 @@
+#$Header: 
+def generate(env, **kw):
+    if not kw.get('depsOnly', 0):
+        env.Tool('addLibrary', library = ['calibTkrUtil'])
+    env.Tool('addLibrary', library = env['rootLibs'])
+    env.Tool('addLibrary', library = env['rootGuiLibs'])
+    env.Tool('commonRootDataLib')
+    env.Tool('reconRootDataLib')
+    env.Tool('digiRootDataLib')
+def exists(env):
+    return 1;
